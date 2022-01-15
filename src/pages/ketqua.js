b5fc167baa7807ab65845e00a578bf2be01385e2
Kyryu <lolnhoxvtpn@gmail.com>
2022-01-15 15:42:40 +0700

    15h43 - 15.01.2022

--- src/pages/ketqua.js
+++ src/pages/ketqua.js
@@ -1,7 +1,6 @@
 import React,{useState, useEffect} from "react";
 import * as bootstrap from "bootstrap";
 import _ from 'lodash';
-import QRCode from 'qrcode.react'
 import { useParams } from "react-router-dom";
 
 function KetQua(props){
